1d068d7e275842a6a05f6d5bda9a3d2276dd0964
Yogesh Kumar <yogesh_kumar@epam.com>
2020-01-30 19:31:06 +0530

    Update scope

---
+++ __init__.py
@@ -0,0 +1,17 @@
+"""
+flask vs django
+no boilerplate code - has boilerplate code
+flexible - MVT
+runs from app.py - settings.py
+simple routes - urls.py
+SQLAlchemy ORM - django ORM
+flask-restful - drf
+
+marshmallow for serialization
+wsgi server for flask
+explore options for middleware
+Authentication and session management
+Jinja templates
+flask-script
+flask-migrate
+"""
