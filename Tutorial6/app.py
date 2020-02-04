1de2ae8b6533a859cc7cdbd34b4c5dac97abeea7
Yogesh Kumar <yogesh_kumar@epam.com>
2020-02-04 17:56:13 +0530

    Update to add middleware to specific blueprint

--- Tutorial6/app.py
+++ Tutorial6/app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 
 def before():
+    print('Run Before Request')
     if 'Authorization' not in request.headers:
         return jsonify({'message': "Unauthorized Access"}), 401
 
@@ -15,6 +16,7 @@ def after(req):
 
 
 bp = Blueprint('web', __name__)
+bp2 = Blueprint('greet', __name__)
 
 
 def index():
@@ -23,8 +25,17 @@ def index():
     return f"{request.query_string} ======== {request}"
 
 
+def greet(name=None):
+    if not name:
+        return '<h1> Hello World! </h1>'
+    return f'<h1> Hello {name.title()}! </h1>'
+
 bp.add_url_rule('/', 'index', index)
 
+bp2.add_url_rule('/greet/', 'greet', greet)
+bp2.add_url_rule('/greet/<string:name>', 'greet_user', greet)
+
 app.register_blueprint(bp)
-app.before_request_funcs = {None: [before, ]}
+app.register_blueprint(bp2)
+app.before_request_funcs = {'web': [before, ]}
 app.after_request(after)
